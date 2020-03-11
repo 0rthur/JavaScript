247a6a67b0f51794873ea6c8ef12351b90ff675e
0rthur <artur03ussenko@gmail.com>
2020-03-11 13:47:10 +0200

    lol

---
+++ Accounting/accounting.js
@@ -0,0 +1,17 @@
+const MAX_EXPENSES = 150;
+let currentExpenses = 0;
+
+function addExpense(description, price) {
+  if (price < 0) {
+    return false;
+  } else {
+
+  }
+
+  if (currentExpenses + price <= MAX_EXPENSES) {
+    currentExpenses += price;
+    return true;
+  } else {
+    return false;
+  }
+}
